87643418b1a30d7529a7528d2b26caa8510aad78
Karim Akra <144803230+KarimAkra@users.noreply.github.com>
2024-11-07 12:57:33 +0200

    Create ScreenUtils.hx

---
+++ Source/external/ScreenUtils.hx
@@ -0,0 +1,10 @@
+package external;
+
+@:build(LinkerMacro.xml('project/Build.xml'))
+@:include('ScreenUtils.hpp')
+@:unreflective
+extern class ScreenUtils
+{
+  @:native('getSafeAreaInsets')
+  static function getSafeAreaInsets(top:cpp.RawPointer<Float>, bottom:cpp.RawPointer<Float>, left:cpp.RawPointer<Float>, right:cpp.RawPointer<Float>):Void;
+}
